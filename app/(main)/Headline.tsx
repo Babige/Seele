03d7527c69ce40aefd478b289569b5efc5f70c61
Babige <68818361+Babige@users.noreply.github.com>
2024-12-14 10:57:42 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
 
-import { SparkleIcon, UserSecurityIcon } from '~/assets'
+import { SparkleIcon, UserSecurityIcon， BrushIcon， HammerIcon， LightBulbIcon } from '~/assets'
 import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { SocialLink } from '~/components/links/SocialLink'
 
@@ -11,8 +11,8 @@ function Developer() {
   return (
     <span className="group">
       <span className="font-mono">&lt;</span>开发者
-      <span className="font-mono">/&gt;</span>
-      <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
+      <LightBulbIcon className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
+      <span class="ml-1 mr-2">工程师</span>
     </span>
   )
 }
@@ -26,25 +26,29 @@ function Designer() {
         <span className="absolute -bottom-[3.5px] -left-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
         <span className="absolute -right-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
       </span>
-      设计师
+      {/* 图标和文字一起显示 */}
+      <span className="group">
+        <BrushIcon className="mr-2 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
+        <span class="ml-1 mr-2">设计师</span>
+      </span>
     </span>
   )
 }
 
 function OCD() {
   return (
-    <span className="group inline-flex items-center">
+    <span className="group">
       <SparkleIcon className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
-      <span>细节控</span>
+      <span class="ml-1 mr-2">细节控</span>
     </span>
   )
 }
 
 function Founder() {
   return (
-    <span className="group inline-flex items-center">
+    <span className="group ml-2">
       <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
-      <span>创始人</span>
+      <span class="ml-1 mr-2">创始人</span>
     </span>
   )
 }
@@ -63,7 +67,7 @@ export function Headline() {
           duration: 0.3,
         }}
       >
-        <Developer />，<Designer />，
+        <Developer />，<Designer />
         <span className="block h-2" />
         <OCD />，<Founder />
       </motion.h1>
@@ -80,10 +84,10 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 Cali，
-          <PeekabooLink href="https://zolplay.com">佐玩</PeekabooLink>
-          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
-          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
+          我是 Seele，
+          <PeekabooLink href="https://pimpi.im">Pimpi</PeekabooLink>
+          创始人，目前带领着Pimpi致力于完成一项充满创造力的工作，同时鼓励团队创造影响世界的产品。
+          我热爱设计，创新，享受生活，以及在未知领域中探索。
         </Balancer>
       </motion.p>
       <motion.div
@@ -99,33 +103,33 @@ export function Headline() {
         }}
       >
         <SocialLink
-          href="https://cali.so/twitter"
+          href="https://seele.vercel.app/twitter"
           aria-label="我的推特"
           platform="twitter"
         />
         <SocialLink
-          href="https://cali.so/youtube"
+          href="https://seele.vercel.app/youtube"
           aria-label="我的 YouTube"
           platform="youtube"
         />
         <SocialLink
-          href="https://cali.so/bilibili"
+          href="https://seele.vercel.app/bilibili"
           aria-label="我的 Bilibili"
           platform="bilibili"
         />
         <SocialLink
-          href="https://cali.so/github"
+          href="https://seele.vercel.app/github"
           aria-label="我的 GitHub"
           platform="github"
         />
         <SocialLink
-          href="https://cali.so/tg"
+          href="https://seele.vercel.app/tg"
           aria-label="我的 Telegram"
           platform="telegram"
         />
         <SocialLink href="/feed.xml" platform="rss" aria-label="RSS 订阅" />
         <SocialLink
-          href="mailto:hi@cali.so"
+          href="mailto:yunzhi_hu@126.com"
           aria-label="我的邮箱"
           platform="mail"
         />
