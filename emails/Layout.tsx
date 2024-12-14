d2499eb7e43fbe930e8c505e6ec61b5f65c51fa2
Babige <68818361+Babige@users.noreply.github.com>
2024-12-14 11:40:46 +0800

    Update Layout.tsx

--- emails/Layout.tsx
+++ emails/Layout.tsx
@@ -39,43 +39,43 @@ export default function Layout({
                 src={`${emailConfig.baseUrl}/icon.png`}
                 width="24"
                 height="24"
-                alt="Cali"
+                alt="Seele"
                 className="mx-auto my-0"
               />
               <Text className="text-center">
                 <Link
-                  href="https://cali.so"
+                  href="https://seele.vercel.app"
                   className="text-zinc-700 underline"
                 >
-                  <strong>Cali Castle</strong>
+                  <strong>Seele Hu</strong>
                 </Link>
                 <br />
-                开发者、设计师、细节控、创始人
+                工程师、设计师、细节控、创始人
               </Text>
               <Text className="text-center">
                 <Link
-                  href="https://cali.so/twitter"
+                  href="https://seele.vercel.app/twitter"
                   className="text-xs text-zinc-600 underline"
                 >
                   Twitter
                 </Link>{' '}
                 |&nbsp;
                 <Link
-                  href="https://cali.so/youtube"
+                  href="https://seele.vercel.app/youtube"
                   className="text-xs text-zinc-600 underline"
                 >
                   YouTube
                 </Link>{' '}
                 |&nbsp;
                 <Link
-                  href="https://cali.so/github"
+                  href="https://seele.vercel.app/github"
                   className="text-xs text-zinc-600 underline"
                 >
                   GitHub
                 </Link>{' '}
                 |&nbsp;
                 <Link
-                  href="https://cali.so/bilibili"
+                  href="https://seele.vercel.app/bilibili"
                   className="text-xs text-zinc-600 underline"
                 >
                   哔哩哔哩
