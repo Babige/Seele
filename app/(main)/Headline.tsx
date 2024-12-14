ed14c02785faa989accc90fe0676ab131447c222
Babige <68818361+Babige@users.noreply.github.com>
2024-12-14 14:44:57 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -9,16 +9,14 @@ import { SocialLink } from '~/components/links/SocialLink'
 
 function Developer() {
   return (
-    <span className="group relative rounded-2xl bg-black/5 p-1 dark:bg-white/5">
+    <span className="group">
       <svg
-        className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180"
         width="1em"
         height="1em"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        role="img"
-        aria-hidden="true"
+        className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180"
       >
         <path
           d="M3 12H4M12 3V4M20 12H21M5.6 5.6L6.3 6.3M18.4 5.6L17.7 6.3M9.70001 17H14.3M9 16C8.16047 15.3704 7.54033 14.4925 7.22743 13.4908C6.91453 12.4892 6.92473 11.4144 7.25658 10.4189C7.58844 9.4233 8.22512 8.55739 9.07645 7.94379C9.92778 7.33019 10.9506 7 12 7C13.0494 7 14.0722 7.33019 14.9236 7.94379C15.7749 8.55739 16.4116 9.4233 16.7434 10.4189C17.0753 11.4144 17.0855 12.4892 16.7726 13.4908C16.4597 14.4925 15.8395 15.3704 15 16C14.6096 16.3865 14.3156 16.8594 14.1419 17.3806C13.9681 17.9018 13.9195 18.4566 14 19C14 19.5304 13.7893 20.0391 13.4142 20.4142C13.0391 20.7893 12.5304 21 12 21C11.4696 21 10.9609 20.7893 10.5858 20.4142C10.2107 20.0391 10 19.5304 10 19C10.0805 18.4566 10.0319 17.9018 9.85813 17.3806C9.6844 16.8594 9.39043 16.3865 9 16Z"
@@ -30,7 +28,7 @@ function Developer() {
       </svg>
       <span className="ml-1 mr-2">工程师</span>
     </span>
-  );
+  )
 }
 
 function Designer() {
@@ -60,7 +58,7 @@ function Designer() {
             strokeLinejoin="round"
           />
         </svg>
-        <span className="ml-1 mr-2">设计师</span>
+        <span className="ml-1 mr-2">照明设计师</span>
       </span>
     </span>
   );
@@ -79,7 +77,7 @@ function Founder() {
   return (
     <span className="group ml-2">
       <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
-      <span className="ml-1 mr-2">创始人</span>
+      <span className="ml-1 mr-2">Pimpi创始人</span>
     </span>
   )
 }
