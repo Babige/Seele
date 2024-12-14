1808551afe77b61dbfbbdbffc6ba1a244fc8c574
Babige <68818361+Babige@users.noreply.github.com>
2024-12-14 15:47:51 +0800

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -4,7 +4,7 @@ export const seo = {
     '我叫 Seele，一名工程师，设计师，细节控，同时也是Pimpi创始人，目前带领着Pimpi致力于完成一项充满创造力的工作，同时鼓励团队创造影响世界的产品。',
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://seele.vercel.app'
+      ? 'https://seele.im'
       : 'http://localhost:3000'
   ),
 } as const
